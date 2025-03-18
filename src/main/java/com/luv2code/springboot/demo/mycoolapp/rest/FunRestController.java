792d837db665adb796ac1139032e167ae730ed8b
MirunaCeccar <miruna.buruiana@ceccar.ro>
2025-03-18 14:10:29 +0200

    modiefied properties

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -5,9 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-
+    //expose a new endpoint for greeting
     @GetMapping("/")
     public String sayHello(){
         return "Hello World!";
+    }
+    //expose a new endpoint for workout
+    @GetMapping("/workout")
+    public String doSomeWork(){
+        return "Hips thrust";
+    }
+
+    //expose a new endpoint for fortune
+    @GetMapping("/fortune")
+    public String getLucky(){
+        return "You won a 1000 dollars!";
     }
 }
